0424c59a43aa257a86ffc1499835d6ff0d0971b2
chudecjusz <32513866+chudecjusz@users.noreply.github.com>
2017-10-13 13:51:20 +0200

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello word!!! [SA]");
+            Console.WriteLine("Hello word!!! [SA] dupa");
             Console.WriteLine("Ala ma kota");
             Console.ReadKey();
         }
